3709ecb034144748378f9f2c33d83d1793018124
katshaze <katrinamargaret.hayes@gmail.com>
2018-06-28 13:46:04 +1000

    feat: link up actions, components, state

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
+    console.log(this.props.posts);
+
     return (
       <div>
         Posts Index
@@ -10,4 +19,14 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+// putting {fetchPosts} here is a shortcut for gaining access to this action creator
+// now have access to this.props.fetchPosts inside of the component.
+// null is for where the match state to props argument would have gone
+
+// componentWillMount() is another option, but either way the component will try to render
+// while the request for data is sent off. as it's all async.
--- src/index.js
+++ src/index.js
@@ -24,3 +24,4 @@ ReactDOM.render(
 // Browser Router: want react router to look at the entire url (cf. hash router) when deciding what components to render on the screen. Interacts with that history library from the react router workflow diagram.
 
 // Route component (imported from react-router-dom) is an object that is a react component that we can render inside of any other react component that we put together inside of our app. provides the configuratin of 'if url like x, show y component; if url like z, show a component'. Always must have two properties: a path and a component
+
